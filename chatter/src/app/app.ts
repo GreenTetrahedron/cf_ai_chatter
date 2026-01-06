6d893d42d7d65468e52c7b0fdc0b3bee7698fd1a
Hamza Maniar <maniarhamza37@gmail.com>
2026-01-06 20:21:58 +0000

    The app is now sending a basic request to a basic worker on Cloudflare

--- chatter/src/app/app.ts
+++ chatter/src/app/app.ts
@@ -1,4 +1,5 @@
-import { Component, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,5 +9,19 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('chatter');
+  request = { "message": "Hello, World!" };
+
+  client = inject(HttpClient);
+
+  response: any = signal(null);
+
+  ngOnInit() { 
+    console.log(this.request);
+
+    this.client.post("https://test-worker.maniarhamza01.workers.dev/", JSON.stringify(this.request))
+      .subscribe(thing => {
+        console.log(thing);
+        this.response.set(JSON.stringify(thing));
+      });
+  }
 }

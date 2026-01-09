f6e8865838bd31e2d9172be455542dcb6030623a
Hamza Maniar <maniarhamza37@gmail.com>
2026-01-09 01:04:40 +0000

    Build and Deployment configurations

--- chatter/src/app/app.routes.ts
+++ chatter/src/app/app.routes.ts
@@ -3,17 +3,16 @@ import { Home } from './pages/home/home';
 
 export const routes: Routes = [
     {
-        path: '',
-        redirectTo: 'chat/' + crypto.randomUUID(),  
-        pathMatch: 'full'
-    },
-    {
         path: 'chat/:id',
         component: Home
     },
     {
-        path: '**',
+        path: '',
         redirectTo: 'chat/' + crypto.randomUUID(),
-        pathMatch: 'prefix'
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        redirectTo: 'chat/' + crypto.randomUUID()
     }
 ];
